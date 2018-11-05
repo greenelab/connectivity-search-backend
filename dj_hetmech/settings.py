11f95d4231172af52939b4fc3c0075fddbb66ffc
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2018-11-05 16:49:09 -0500

    Switch database to postgres

--- dj_hetmech/settings.py
+++ dj_hetmech/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'dj_hetmech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dj_hetmech',
+        'USER': 'dj_hetmech',
+        'PASSWORD': 'not_secure',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
