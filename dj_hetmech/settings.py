greenelab/connectivity-search-backend

commit e72b7fc665f7ad7da0756a9dcaccc2cb0b4ac188

Add more confidentials to secrets template
Merges https://github.com/greenelab/hetmech-backend/pull/12

dj_hetmech/settings.py
@@ -25,9 +25,6 @@ with open(path) as read_file:
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret_not_yet_set'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -84,11 +81,11 @@ WSGI_APPLICATION = 'dj_hetmech.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dj_hetmech',
-        'USER': 'read_only_user',
+        'NAME': secrets['db']['name'],,
+        'USER': secrets['db']['user'],,
         'PASSWORD': secrets['db']['password'],
-        'HOST': 'phr-greene-007.med.upenn.edu',
-        'PORT': 5432,
+        'HOST': secrets['db']['host'],
+        'PORT': secrets['db']['port'],
     }
 }
 

dj_hetmech/secrets-template.yml
@@ -1,2 +1,6 @@
 db:
+  name: database_name
+  user: db_user
   password: not_secure
+  host: database_hostname
+  port: database_port_number

commit 7c45ca4aa306765fc4eae91ef0180a8631aada8b

Fix errors in setup.py
Reverts errors introduced in
https://github.com/greenelab/hetmech-backend/commit/e72b7fc665f7ad7da0756a9dcaccc2cb0b4ac188

SECRET_KEY is not for the database and must exist or Django will
not start up.
https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-SECRET_KEY

Removes duplicate commas

dj_hetmech/settings.py
@@ -25,6 +25,9 @@ with open(path) as read_file:
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'secret_not_yet_set'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -81,8 +84,8 @@ WSGI_APPLICATION = 'dj_hetmech.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': secrets['db']['name'],,
-        'USER': secrets['db']['user'],,
+        'NAME': secrets['db']['name'],
+        'USER': secrets['db']['user'],
         'PASSWORD': secrets['db']['password'],
         'HOST': secrets['db']['host'],
         'PORT': secrets['db']['port'],