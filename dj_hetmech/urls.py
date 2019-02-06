015fd8829bd77037c38239c48039c620dec1c14c
dongbohu <dongbo.hu@gmail.com>
2019-02-06 10:49:13 -0500

    Web API for Node search
    Merges https://github.com/greenelab/hetmech-backend/pull/17
    
    * Simple REST API for node search
    * Remove frontend test
    * Add comments on DRF filters, remove unnecessary import

--- dj_hetmech/urls.py
+++ dj_hetmech/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from rest_framework import routers
+from dj_hetmech_app import views
+
+router = routers.DefaultRouter()
+router.register("nodes", views.NodeView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
 ]
--- dj_hetmech_app/views.py
+++ dj_hetmech_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
+
+from .models import Node
+from .serializers import NodeSerializer
+
 
 # Create your views here.
+
+# The view that shows node information.
+# See this page for "search" implementation and other filter options:
+# https://www.django-rest-framework.org/api-guide/filtering/
+class NodeView(ModelViewSet):
+    http_method_names = ['get']
+
+    queryset = Node.objects.all()
+    serializer_class = NodeSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('identifier', 'metanode__identifier', 'name')
