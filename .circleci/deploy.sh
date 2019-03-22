d1bc2ce7d8d311acc1350a8463dfdae56cac857c
Dongbo <dongbo.hu@gmail.com>
2019-03-22 10:13:07 -0400

    Update conda when environment.yml changes

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-cd ~ubuntu/hetmech-backend
-git checkout master; git pull
+cd ~/hetmech-backend
+git checkout master
+
+ENV_FILE="environment.yml"
+git remote update
+ENV_DIFF=`git diff origin $ENV_FILE`
+git pull
+
+# Update conda env if needed
+if [ -n "$ENV_DIFF" ]; then
+    conda update --file $ENV_FILE
+fi
+
+# Restart Gunicorn daemon
 sudo /etc/init.d/supervisor restart
