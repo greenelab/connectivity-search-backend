9867e1111c679c4da0b278f06fd192a2fdc14db3
Dongbo <dongbo.hu@gmail.com>
2019-03-28 12:47:34 -0400

    conda tweak

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -2,7 +2,7 @@
 
 # This script should be run as user "ubuntu" only.
 if [ `whoami` != 'ubuntu' ]; then
-    echo "Error: current user is not ubuntu!"
+    echo "Error: current user is not 'ubuntu'"
     exit
 fi
 
@@ -24,28 +24,32 @@ sudo certbot --nginx certonly
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo cp hetmech-api.conf /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/hetmech-api.conf /etc/nginx/sites-enabled/
-sudo /etc/init.d/nginx restart
-
-# Miniconda environment setup
-sudo apt install git wget python3 python3-pip --yes
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --output-document miniconda.sh
-bash miniconda.sh -b -p ~/miniconda
-cat bashrc_conda >> ~/.bashrc && source ~/.bashrc
-apt install gcc --yes  # gcc is required by "pip install git+https://..."
-
-cd && git clone https://github.com/greenelab/hetmech-backend.git
-cd hetmech-backend
-conda env create --quiet --file environment.yml
-conda activate hetmech-backend
-
-# "static" directory (used by API view in HTML format)
-mkdir -p ~/www/static && chmod -R 755 ~/www/
-python manage.py collectstatic
-
-python manage.py makemigrations dj_hetmech_app
-python manage.py migrate
 
 # Use supervisord to take care of Gunicorn daemon
 sudo apt install supervisor --yes
 sudo cp gunicorn.conf /etc/supervisor/conf.d/
+
+# Miniconda environment setup
+sudo apt install git wget python3 python3-pip gcc --yes
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh --output-document ~/miniconda.sh
+bash ~/miniconda.sh -b -p ~/miniconda
+echo "source ~/miniconda/etc/profile.d/conda.sh" >> ~/.bashrc
+
+rm -rf ~/hetmech-backend
+git clone https://github.com/greenelab/hetmech-backend.git ~/hetmech-backend
+source ~/miniconda/etc/profile.d/conda.sh
+conda env create --quiet --file ~/hetmech-backend/environment.yml
+conda activate hetmech-backend
+
+# Build "static" directory (used by API view in HTML format)
+mkdir -p ~/www/static && chmod 755 ~/www/ ~/www/static
+
+# Copy "secrets.yml" into ~/hetmech-backend/dj_hetmech/, then continue
+cd ~/hetmech-backend
+python manage.py collectstatic --clear --no-input
+python manage.py makemigrations dj_hetmech_app
+python manage.py migrate
+
+# Restart Gunicorn and Nginx
 sudo /etc/init.d/supervisor restart
+sudo /etc/init.d/nginx restart
