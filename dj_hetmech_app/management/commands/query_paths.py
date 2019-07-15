3377f70f22319a6f7a50a17e3e33748d3c082818
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-07-15 13:10:59 -0400

    Revert changes to query-paths management command

--- dj_hetmech_app/management/commands/query_paths.py
+++ dj_hetmech_app/management/commands/query_paths.py
@@ -4,34 +4,25 @@ import hetnetpy.readwrite
 from django.core.management.base import BaseCommand
 
 from dj_hetmech_app.utils.paths import (
+    get_neo4j_node_info,
+    get_neo4j_rel_info,
     get_paths,
-    get_metapath_queryset,
 )
 from dj_hetmech_app.models import Node
-
 
 class Command(BaseCommand):
 
     help = 'Call dj_hetmech_app.utils.paths.get_paths for prototyping purposes.'
 
     def handle(self, *args, **options):
-        # source_node = Node.objects.get(metanode='Compound', identifier='DB01156')  # Bupropion
-        # target_node = Node.objects.get(metanode='Disease', identifier='DOID:0050742')  # nicotine dependency
-        # json_obj = get_paths(
-        #     #metapath='CbGiGaD',
-        #     metapath='CbGiGaDrD',
-        #     source_id=source_node.id, 
-        #     target_id=target_node.id,  
-        #     limit=100,
-        # )
-        qs = get_metapath_queryset(
-            source_metanode='Gene',
-            target_metanode='Disease',
+        source_node = Node.objects.get(metanode='Compound', identifier='DB01156')  # Bupropion
+        target_node = Node.objects.get(metanode='Disease', identifier='DOID:0050742')  # nicotine dependency
+        json_obj = get_paths(
+            #metapath='CbGiGaD',
+            metapath='CbGiGaDrD',
+            source_id=source_node.id, 
+            target_id=target_node.id,  
+            limit=100,
         )
-        qs = qs[:5]
-        from dj_hetmech_app.serializers import MetapathSerializer, serialize_record
-        #json_obj = [serialize_record(record, extra_fields=['reversed']) for record in qs]
-        json_obj = [MetapathSerializer(record).data for record in qs]
-        #json_obj = list(qs.values())
-        json_str = json.dumps(json_obj, indent=2, ensure_ascii=False)
+        json_str = json.dumps(json_obj, indent=2)
         print(json_str)
